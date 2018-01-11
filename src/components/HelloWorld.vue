5f59816c5d6a74e8653d08ed4f5665f236984b0c
BatyaHack <officalbatya@gmail.com>
2018-01-11 18:00:52 +0200

    vuex store

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}123</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -24,8 +24,9 @@
 export default {
   name: 'HelloWorld',
   data () {
+    console.log(store);
     return {
-      msg: 'Hello, world'
+      msg: store.state.todos,
     }
   }
 }
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as actions from './actions'
+
+Vue.use(Vuex);
+
+
+export default new Vuex.Store({
+  state: {
+    todos: [
+      {id: 1, text: 'Hello, world', done:true}
+    ]
+  }
+})
