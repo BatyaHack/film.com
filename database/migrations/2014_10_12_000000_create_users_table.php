2256695d62b300fc10f838e4c1366e1410e8fc81
BatyaHack <officalbatya@gmail.com>
2018-04-03 01:17:00 +0300

    avatar for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->string('token');
+            $table->string('avatar')->default('avatar.png');
         });
     }
 
